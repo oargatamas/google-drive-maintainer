4d0dcd85afede99114755a461414b9c15f93d081
tamas.oarga <tamas.oarga@istructure.com>
2024-12-13 14:34:46 +0100

    Legacy codes removed.

--- src/utils/drive_utils.js
+++ src/utils/drive_utils.js
@@ -30,3 +30,25 @@ function getParents(driveItem) {
 
     return parents.reverse()
 }
+
+function backupDriveFolder(folderToBackup, backupFolder) {
+    console.log("Creating backup from folder " + folderToBackup.getName());
+    const trash = DriveApp.getFolderById("1EvJWH9jVwOTwNdusxirbkgOXXpdmAA69");
+
+    const files = folderToBackup.getFiles();
+    while(files.hasNext()){
+        const file = files.next();
+        backupDriveFile(file, backupFolder, trash);
+    }
+    console.log("Backup for " + folderToBackup.getName() + " finished successfully.");
+}
+
+function backupDriveFile(file, backupFolder){
+    const id = file.getId();
+
+    if(!backupFolder.getFilesByName(file.getName()).hasNext()){
+        console.log("'"+file.getName()+"' NOT existing in backup folder. Copying it now.");
+        const backupCopy = makeItemPrivate(file.makeCopy(backupFolder));
+        console.log(id + "| Backup file id: " + backupCopy.getId());
+    }
+}
